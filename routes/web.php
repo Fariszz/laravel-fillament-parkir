36bf41c1cdad3d5c55de1d83f0aa08fd1ab8de6c
Muhammad Faris Hadi Mulyo <kimfais1234@gmail.com>
2024-09-20 21:33:09 +0700

    feat(no-ticket): add landing page

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return redirect()->route('admin.login');
-    return redirect()->to('/admin/login');
+    // return redirect()->to('/admin/login');
+    return view('landing');
 });
